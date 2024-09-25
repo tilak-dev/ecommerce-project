d85197cd3ac7e9384bbacec2e8b9c89fa451891b
Tilak Singh <tsingh99991@gmail.com>
2024-09-25 11:52:03 +0530

    CRUD operations done for category

---
+++ src/app/api/category/delete-category/[categoryId]/route.ts
@@ -0,0 +1,51 @@
+import dbConnect from "@/configs/dbconnect";
+import CategoryModel from "@/models/Category";
+import { NextRequest, NextResponse } from "next/server";
+import slugify from "slugify";
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { categoryId: string } }
+) {
+  dbConnect(); //necessary for bd connect
+  try {
+    const { categoryId } = params;
+    if (!categoryId) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Please provide Category Id",
+        },
+        { status: 400 }
+      );
+    }
+    const deletedcategory = await CategoryModel.findByIdAndDelete(categoryId);
+    if (!deletedcategory) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Category not found or already deleted",
+        },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Category Deleted successfully",
+        data: deletedcategory,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("error in delete Category", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error in delete Category",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/category/single-category/[categoryId]/route.ts
@@ -0,0 +1,54 @@
+import dbConnect from "@/configs/dbconnect";
+import CategoryModel from "@/models/Category";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { categoryId: string } }
+) {
+  dbConnect(); //necessary for bd connect
+
+  const { categoryId } = params;
+  try {
+    //validate the categoryId
+    if (!categoryId) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "categoryId is required",
+        },
+        { status: 400 }
+      );
+    }
+    //getting category
+    const category = await CategoryModel.findById(categoryId);
+    if (!category) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: " error in finding Category",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Category found successfully",
+        data: category,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("error in single Category", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error in single Category",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
