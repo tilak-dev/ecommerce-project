ad2ea105cc35324fc50ce4e323dcde9366b7bd1c
Tilak Singh <tsingh99991@gmail.com>
2024-09-28 11:10:18 +0530

    get user order route and delete user order route added

---
+++ src/app/api/user-order/cencel-order/[orderId]/route.ts
@@ -0,0 +1,60 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import dbConnect from "@/configs/dbconnect";
+import UserModel from "@/models/User";
+import { getServerSession, User } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+dbConnect();
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { orderId: string } }
+) {
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+  const userId = user._id;
+  try {
+    const { orderId } = params;
+    if (!userId || !orderId) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "adress and user is not found",
+        },
+        { status: 404 }
+      );
+    }
+    // check user
+    const result = await UserModel.updateOne(
+      { _id: userId },
+      {
+        $pull: { orders: { _id: orderId } },
+      }
+    );
+
+    if (result.modifiedCount === 0) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "order not fount or already deleted",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      success: true,
+      message: "order deleted successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log("error in deleting order field", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error in deleteing order field",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/user-order/get-order/route.ts
@@ -0,0 +1,54 @@
+import dbConnect from "@/configs/dbconnect";
+import UserModel from "@/models/User";
+import { getServerSession, User } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+dbConnect()
+export async function GET(
+  request: NextRequest,
+) {
+
+  const session = await getServerSession(authOptions)
+
+  const user:User = session?.user as User;
+     const userId = user?._id
+  try {
+   if (!userId){
+    return NextResponse.json(
+      {
+        success: false,
+        message: "User not found",
+      },
+      { status: 404 }
+    );
+   }
+    // check user
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    }
+    
+    return NextResponse.json({
+      success: true,
+      message: "order added successfully",
+      data: user.orders,
+    });
+  } catch (error) {
+    console.log("error in searching orders", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error in order searching",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
